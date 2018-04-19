a08c49d276075754cb62a4c398fbc4c23ed53741
Manuel Cecilio <manuelc.p.g@gmail.com>
2018-04-19 20:10:07 +0200

    FirstIteration
    Demo viable de consultas en tiempo real e historicas de bitcoin

---
+++ src/main/java/ceus/resources/coinDesk/CourrentDataResource.java
@@ -0,0 +1,70 @@
+package ceus.resources.coinDesk;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.restlet.resource.ClientResource;
+
+import ceus.model.coinDesk.Bpi;
+
+public class CourrentDataResource {
+
+	private String uri = "https://api.coindesk.com/v1/bpi/currentprice/";
+
+	/*
+	 * public Double[] getTupleValor(String mon1, String mon2) {
+	 * 
+	 * Double[] res = {}; ClientResource cr = null; mon1.toUpperCase();
+	 * mon2.toUpperCase();
+	 * 
+	 * if(mon1=="USD"||mon2=="USD") { try {
+	 * 
+	 * cr = new ClientResource(uri); Bpi bpi = cr.get(Bpi.class);
+	 * 
+	 * }catch(Exception e) {
+	 * System.err.println("Error al consultar los datos de Coindesk. Params:{"
+	 * +mon1+", "+mon2+"}. Query: "+uri); } }
+	 * 
+	 * return res;
+	 * 
+	 * }
+	 */
+
+	public Double getValor(String mon) {
+		double res = 0.0;
+		ClientResource cr = null;
+		mon.toUpperCase();
+
+		if (mon == "USD") {
+			try {
+				cr = new ClientResource(uri + mon);
+				Bpi bpi = cr.get(Bpi.class);
+				res = new Double(bpi.getUSD().getRate());
+
+			} catch (Exception e) {
+				System.err.println("Error al consultar los datos de Coindesk. Params:{" + mon + "}. Query: " + uri);
+			}
+		} else if (mon == "EUR") {
+			try {
+				cr = new ClientResource(uri + mon);
+				Bpi bpi = cr.get(Bpi.class);
+				res = new Double(bpi.getEUR().getRate());
+
+			} catch (Exception e) {
+				System.err.println("Error al consultar los datos de Coindesk. Params:{" + mon + "}. Query: " + uri);
+			}
+		} else if (mon == "GBP") {
+			try {
+				cr = new ClientResource(uri + mon);
+				Bpi bpi = cr.get(Bpi.class);
+				res = new Double(bpi.getGBP().getRate());
+
+			} catch (Exception e) {
+				System.err.println("Error al consultar los datos de Coindesk. Params:{" + mon + "}. Query: " + uri);
+			}
+		}
+
+		return res;
+	}
+
+}
