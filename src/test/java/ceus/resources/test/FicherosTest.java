934bb0c5c8736265fa4cd26729320dc6a42731ff
Manuel Cecilio <manuelc.p.g@gmail.com>
2018-05-13 09:33:03 +0200

    3.1.1

--- src/test/java/ceus/resources/test/FicherosTest.java
+++ src/test/java/ceus/resources/test/FicherosTest.java
@@ -1,8 +1,16 @@
 package ceus.resources.test;
 
-import org.mcpg.ficheros.UtilFicherosDinamicos;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.mcpg.ficheros.UtilFicheros;
 
 public class FicherosTest {
 
+	@Test
+	public void test1() {
+		boolean r = UtilFicheros.escribeFicheroCompleto("./ficheros/test1.txt", "Prueba 3", "##");
+		assertTrue(r);
+	}
 	
 }
